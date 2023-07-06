f5dcf27b3ad735d58a91f0e6260a36a778365776
Stas Uzunov <SSUzunov19@codingburgas.bg>
2023-07-06 12:23:45 +0300

    Remove config.js

--- server/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    db: {
-        host: 'localhost',
-        user: 'postgres',
-        port: 5432,
-        password: 'czMz987Ni6FlG6&wLSH*bbaVAZTU!4',
-        database: 'aibest'
-    }
-}
