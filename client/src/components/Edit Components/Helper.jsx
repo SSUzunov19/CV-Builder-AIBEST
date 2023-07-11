0958f5121434f9fba635c57a08ffd85395a2ac16
Stas Uzunov <SSUzunov19@codingburgas.bg>
2023-07-11 18:35:34 +0300

    Add helper for data in cv builder

---
+++ client/src/components/Edit Components/Helper.jsx
@@ -0,0 +1,31 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { Card, CardContent, Typography, Box, Grid } from '@mui/material';
+
+import SetEmpty from "../UI Components/SetEmpty";
+import SetSample from "../UI Components/SetSample";
+
+const SettingMain = () => {
+    return (
+        <AnimatePresence>
+            <motion.div layout className="cardStyle">
+                <Card>
+                    <CardContent>
+                        <Typography variant="h3" component="div">CV Builder</Typography>
+                        <Box mt={4}>
+                            <Grid container spacing={2}>
+                                <Grid item>
+                                    <SetEmpty />
+                                </Grid>
+                                <Grid item>
+                                    <SetSample />
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </CardContent>
+                </Card>
+            </motion.div>
+        </AnimatePresence>
+    );
+};
+
+export default SettingMain;
