995442308d4488c272c680deaab9bb0757f3fe8c
MMPetrov20 <MMPetrov20@codingburgas.bg>
2023-07-13 12:59:07 +0300

    Implement back and confirm buttons

--- client/src/components/TargetTemplate/TargetTemplate.js
+++ client/src/components/TargetTemplate/TargetTemplate.js
@@ -1,4 +1,6 @@
 import React from "react";
+import backButton from "../../images/buttons/Back button.png"
+import confirmButton from "../../images/buttons/Confirm button.png"
 import "./TargetTemplate.css"
 
 export default function TargetTemplate({ selectedTemplate }) {
@@ -9,9 +11,11 @@ export default function TargetTemplate({ selectedTemplate }) {
 
     return (
         <div className="target-template-container">
+            <img src={backButton} id="back-button" alt="back button" ></img>
             <div className="target-template">
-                <img src={getImageFilePath()} className="selected-template" alt=""></img>
+                <img src={getImageFilePath()} className="selected-template" alt="selected template"></img>
             </div>
+            <img src={confirmButton} id="confirm-button" alt="confirm button"></img>
         </div>
     )
 }
