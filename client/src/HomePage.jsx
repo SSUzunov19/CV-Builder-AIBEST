d7c80dd50bf41ec270ff9e9a099bf504683f9162
Ivailo Denchev <ivkodenchev2@gmail.com>
2023-07-14 19:11:38 +0300

    add Home Page File,add

---
+++ client/src/HomePage.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+
+const HomePage = () => {
+  return (
+    <>
+    <nav>
+     <div>
+      <button className='navButtonLeft'>Start Creating</button>
+      <button className='navButtonLeft'>Conact Us</button>
+      <button className='navButtonRight'>LogIn</button>
+      <button className='navButtonRight'>Register</button>
+     </div>
+     </nav>
+     
+     <div className='first-section'>
+      <div className='firstSectionImagePH'><img src="https://childrenswi.org/-/media/44dde55c823d4b99a7cf0297244b71b6.ashx?h=150&w=150" alt="CV.mp4" /></div>
+      <div className="firstSectionTextPH">
+     <p className='firstSectionText1'>Build your professional job-winning resume now</p>
+     <p className='firstSectionText2'> Create a modern professional resume fast and easy, <br /> using customizable templates.</p>
+     <button className='Try-Now-BTN' type='Try Now'>Try Now</button>
+      </div>
+     </div>
+     <div className="second-section">
+
+
+
+      
+     </div>
+    </>
+  );
+};
+
+export default HomePage;
