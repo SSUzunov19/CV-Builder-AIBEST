c9b4eae9c088997605873372febca9b58b9974e7
Stas Uzunov <SSUzunov19@codingburgas.bg>
2023-07-20 01:24:21 +0300

    Fix page buttons css

--- client/src/components/PageButtons.jsx
+++ client/src/components/PageButtons.jsx
@@ -3,36 +3,44 @@ import { GoDownload } from "react-icons/go";
 import { BiAnalyse } from "react-icons/bi";
 import { useContext } from "react";
 import { CvContext } from "../hooks/CvContext";
-import { Button, ButtonGroup, Box, CircularProgress } from "@mui/material";
+import { Button, ButtonGroup, Box, CircularProgress, Tooltip } from "@mui/material";
 
 const PageButtons = () => {
     const { scaleUp, scaleDown, ifScaleUpOrDown, analyseTheResume, loading } = useContext(CvContext);
 
     return (
         <Box
-            position="fixed"
-            bottom="20px"
-            right="20px"
-            zIndex={1000}
-            display="flex"
-            justifyContent="center"
+            sx={{
+                position: "fixed",
+                bottom: "20px",
+                right: "20px",
+                zIndex: 1000,
+                display: "flex",
+                justifyContent: "center"
+            }}
         >
-            <Box className="text-neutral-500 mx-auto text-center backdrop-blur-2xl bg-white/50 border border-black/10 px-5 py-3 transition-all rounded-full">
-                <ButtonGroup variant="contained" color="primary" aria-label="contained button group">
-                    <Button onClick={scaleUp}>
-                        <BsZoomIn className="h-8 w-8" />
+            <ButtonGroup variant="contained" color="primary" aria-label="contained button group">
+                <Tooltip title="Zoom In">
+                    <Button onClick={scaleUp} style={{ padding: "8px 18px" }}>
+                        <BsZoomIn style={{ width: "20px", height: "20px" }} />
                     </Button>
-                    <Button onClick={scaleDown}>
-                        <BsZoomOut className="h-8 w-8" />
+                </Tooltip>
+                <Tooltip title="Zoom Out">
+                    <Button onClick={scaleDown} style={{ padding: "8px 18px" }}>
+                        <BsZoomOut style={{ width: "20px", height: "20px" }} />
                     </Button>
-                    <Button onClick={ifScaleUpOrDown}>
-                        <GoDownload className="h-8 w-8" />
+                </Tooltip>
+                <Tooltip title="Download">
+                    <Button onClick={ifScaleUpOrDown} style={{ padding: "8px 18px" }}>
+                        <GoDownload style={{ width: "20px", height: "20px" }} />
                     </Button>
-                    <Button onClick={analyseTheResume}>
-                        {loading ? <CircularProgress color="inherit" size={24} /> : <BiAnalyse className="h-8 w-8" />}
+                </Tooltip>
+                <Tooltip title="Analyse">
+                    <Button onClick={analyseTheResume} style={{ padding: "8px 18px" }}>
+                        {loading ? <CircularProgress color="inherit" size={24} /> : <BiAnalyse style={{ width: "20px", height: "20px" }} />}
                     </Button>
-                </ButtonGroup>
-            </Box>
+                </Tooltip>
+            </ButtonGroup>
         </Box>
     );
 };
