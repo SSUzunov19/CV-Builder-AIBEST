7900a2976ad7f0b4a8ebf754f98fc4475b3276c8
Stas Uzunov <SSUzunov19@codingburgas.bg>
2023-07-19 19:41:17 +0300

    Fix Navbar navigate routes

--- client/src/components/Homepage Components/Navbar/Navbar.jsx
+++ client/src/components/Homepage Components/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import "./Navbar.css"
+import { useNavigate } from 'react-router-dom';
 
 export default function Navbar () {
+  const navigate = useNavigate();
   return (
     <div className="navbar-wrapper">
       <nav className="nav-bar">
         <div className="logo">
-          <a href="">Wonder Woman</a>
+          <a href="#" onClick={() => navigate("/")}>Wonder Woman</a>
         </div>
         <div className="account-btns">
-          <a className="nav-bth" href="">Login</a>
-          <a className="nav-bth"href="">Register</a>
+          <a className="nav-bth" href="#" onClick={() => navigate("/login")}>Login</a>
+          <a className="nav-bth" href="#" onClick={() => navigate("/register")}>Register</a>
         </div>
       </nav>
     </div>
