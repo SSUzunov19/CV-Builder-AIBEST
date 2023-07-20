f39df6497e8b5380bae3d08e6b2466cbf6a5020f
Stas Uzunov <SSUzunov19@codingburgas.bg>
2023-07-20 19:53:35 +0300

    Add some css to dashboard

--- client/src/components/ResumeDashboard/ResumeDashboard.jsx
+++ client/src/components/ResumeDashboard/ResumeDashboard.jsx
@@ -21,30 +21,6 @@ import template7 from '../../images/templates/templatee7.png';
 import template8 from '../../images/templates/templatee8.png';
 import template9 from '../../images/templates/templatee9.png';
 
-function GradientIcon({ colorShades }) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="79" height="78" viewBox="0 0 79 78" fill="none">
-      <g clip-path="url(#clip0_182_306)">
-        <path d="M0.493164 69.5393C0.493164 74.0661 4.17812 77.7351 8.68902 77.7351H70.0308C74.5576 77.7351 78.2267 74.0502 78.2267 69.5393V8.19744C78.2267 3.67066 74.5417 0.00158691 70.0308 0.00158691H8.68902C4.16224 0.00158691 0.493164 3.68654 0.493164 8.19744V69.5393ZM70.0308 73.8437H8.68902C6.32239 73.8437 4.38461 71.9059 4.38461 69.5393V59.2945L19.1244 44.5546L31.72 57.1502C32.4824 57.9126 33.7054 57.9126 34.4678 57.1502L57.2129 34.4051L74.3353 51.5275V69.5393C74.3353 71.9059 72.3975 73.8437 70.0308 73.8437ZM8.68902 3.89303H70.0308C72.3975 3.89303 74.3353 5.83081 74.3353 8.19744V46.0159L58.5789 30.2754C57.8165 29.513 56.5935 29.513 55.8311 30.2754L33.086 53.0205L20.4904 40.4249C19.728 39.6625 18.505 39.6625 17.7426 40.4249L4.38461 53.7829V8.19744C4.38461 5.83081 6.32239 3.89303 8.68902 3.89303Z" fill="url(#paint0_linear_182_306)" />
-        <path d="M24.5883 31.149C30.0522 31.149 34.4836 26.7017 34.4836 21.2537C34.4836 15.8056 30.0363 11.3583 24.5883 11.3583C19.1402 11.3583 14.6929 15.8056 14.6929 21.2537C14.6929 26.7017 19.1243 31.149 24.5883 31.149ZM24.5883 15.2497C27.9079 15.2497 30.5922 17.9499 30.5922 21.2537C30.5922 24.5574 27.892 27.2576 24.5883 27.2576C21.2845 27.2576 18.5843 24.5574 18.5843 21.2537C18.5843 17.9499 21.2686 15.2497 24.5883 15.2497Z" fill="url(#paint1_linear_182_306)" />
-      </g>
-      <defs>
-        <linearGradient id="paint0_linear_182_306" x1="39.3599" y1="0.00158691" x2="39.3599" y2="77.7351" gradientUnits="userSpaceOnUse">
-          <stop stop-color="#597962" />
-          <stop offset="1" stop-color="#6FAC8E" />
-        </linearGradient>
-        <linearGradient id="paint1_linear_182_306" x1="24.5883" y1="11.3583" x2="24.5883" y2="31.149" gradientUnits="userSpaceOnUse">
-          <stop stop-color="#597962" />
-          <stop offset="1" stop-color="#6FAC8E" />
-        </linearGradient>
-        <clipPath id="clip0_182_306">
-          <rect width="77.7335" height="77.7335" fill="white" transform="translate(0.493164 0.00158691)" />
-        </clipPath>
-      </defs>
-    </svg>
-  );
-}
-
 export const ResumeDashboard = ({ userId, setUserId, userName, setUserName, setResumeId }) => {
   const [resumes, setResumes] = useState([]);
   const [error, setError] = useState(null);
@@ -127,7 +103,6 @@ export const ResumeDashboard = ({ userId, setUserId, userName, setUserName, setR
               className='card-image'
               style={{ backgroundImage: `url(${templates[templateId]})` }}
             >
-              <GradientIcon />
             </div>
 
             <div className='action-btn-container'>
@@ -147,7 +122,9 @@ export const ResumeDashboard = ({ userId, setUserId, userName, setUserName, setR
 
             </div>
 
-            <IconButton size="small" component={Link} to={`/builder/${resume.id}`} onClick={() => setResumeId(resume.id)} className="open-button">Open</IconButton>
+            <IconButton size="small" component={Link} to={`/builder/${resume.id}`} onClick={() => setResumeId(resume.id)} className="open-button">
+              Open
+            </IconButton>
           </div>
         ))}
       </div>
